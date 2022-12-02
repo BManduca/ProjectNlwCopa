768f76347edbdf3f371c01a7b3370649ab0e1fb8
BManduca <brunnomanducarfe@gmail.com>
2022-12-01 23:55:54 -0300

    Update | iniciando desenvolvimento screens find and new | final Aula 03

--- nlw-copa-mobile/mobile/App.tsx
+++ nlw-copa-mobile/mobile/App.tsx
@@ -3,7 +3,8 @@ import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@
 
 import { AuthContextProvider } from "./src/contexts/AuthContext";
 
-import { New } from './src/screens/New';
+// import { New } from './src/screens/New';
+import { Find } from './src/screens/Find';
 import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme'
@@ -23,7 +24,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        { fontsLoaded ? <New/> : <Loading/> }
+        { fontsLoaded ? <Find/> : <Loading/> }
       </AuthContextProvider>
     </NativeBaseProvider>
   );
